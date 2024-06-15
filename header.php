1e08a843fff04eae9fac37f2f74291300a6feb1d
Elyx <151476172+elyxdev@users.noreply.github.com>
2024-06-15 04:29:15 -0500

    navbarupdate

--- header.php
+++ header.php
@@ -22,14 +22,51 @@ if (!isset($_SESSION['lg_in'])) {
       <section class="w-fit"> <!-- Section con el título -->
         <a href="index"><h1 class="text-2xl hover:to-purple-600 hover:from-blue-500 font-bold from-purple-600 to-blue-500 bg-gradient-to-r bg-clip-text text-transparent">Cutpage</h1></a>
       </section>
-      <section class="w-[100%] text-center text-azulnav font-light hidden xl:block"> <!-- Section con los items PC -->
-        <a href="acortar" class="mr-[22px]"><button class="bg-black rounded-full p-[4px] transition duration-200 hover:font-bold hover:text-prpl"><p class="font-medium text-lg pt-[3px]">Acortar</p></button></a>
-        <a href="acortadores" class="mr-[22px]"><button class="bg-black rounded-full p-[4px] transition duration-200 hover:font-bold hover:text-prpl"><p class="font-medium text-lg pt-[3px]">Mis acortadores</p></button></a>
-        <a href="perfil"><button class="bg-black rounded-full p-[4px] transition duration-200 hover:font-bold hover:text-prpl"><p class="font-medium text-lg pt-[3px]">Perfil</p></button></a>
 
-        <a href="login" class="mr-[10px] float-right pt-[6px]"><button class="bg-rosanav rounded-full p-[4px] text-white hover:bg-prpl transition duration-300"><p>Iniciar sesión</p></button></a>
+
+      <section class="w-[100%] text-center text-azulnav font-light hidden xl:block"> <!-- Section con los items PC -->
+      <?php if ($_SESSION['lg_in'] == 1) {
+                // Sesión iniciada
+                // Acortar
+                echo '<a href="acortar" class="mr-[22px]"><button class="bg-black rounded-full p-[4px] transition duration-200 hover:font-bold hover:text-prpl"><p class="font-medium text-lg pt-[3px]">Acortar</p></button></a>';
+                // Acortadores
+                echo '<a href="acortadores" class="mr-[22px]"><button class="bg-black rounded-full p-[4px] transition duration-200 hover:font-bold hover:text-prpl"><p class="font-medium text-lg pt-[3px]">Mis acortadores</p></button></a>';
+                // Perfil
+                echo '<a href="perfil"><button class="bg-black rounded-full p-[4px] transition duration-200 hover:font-bold hover:text-prpl"><p class="font-medium text-lg pt-[3px]">Perfil</p></button></a>';
+                // Cerrar sesión
+                echo '<a href="auth.php?act=logout" class="mr-[10px] float-right pt-[6px]"><button class="bg-malo rounded-full p-[4px] text-white hover:bg-malodo transition duration-300"><p>Cerrar sesión</p></button></a>';
+            } else {
+                // Sesión no iniciada
+                echo '<a href="login" class="mr-[10px] float-right pt-[6px]"><button class="bg-rosanav rounded-full p-[4px] text-white hover:bg-prpl transition duration-300"><p>Iniciar sesión</p></button></a>';
+            }
+      ?>
       </section>
-      
+
+      <!-- Section con los items móvil -->
+      <section class="z-20 bg-grisva w-[80%] h-fit fixed top-[9%] left-[18%] rounded-xl p-6 items-center text-center hidden xl:hidden" id="items">
+      <?php if ($_SESSION['lg_in'] == 1) {
+                // Sesión iniciada
+                // Acortar
+                echo '<a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[25px]">Acortar</button></a>';
+                // Acortadores
+                echo '<a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[25px]">Mis acortadores</button></a>';
+                // Perfil
+                echo '<a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[25px]">Perfil</button></a>';
+                // Cerrar sesión
+                echo '<a href="auth.php?act=logout"><button class="rounded-full bg-malo hover:bg-malodo transtion duration-200 p-1 w-full mb-[5px]">Cerrar sesión</button></a>';
+      } else {
+                // Sesión no iniciada
+                // Iniciar sesión
+                echo '<a href="login"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[5px]">Iniciar sesión</button></a>';
+      }
+      ?>
+        
+        
+        
+        
+
+      </section>
+
       <section class="w-[100%] text-center text-azulnav bg-white-600 font-light block xl:hidden">  <!-- botón movil -->
         <button class="float-right text-white z-10" id="menuhamburguesa"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
           <path fill="#fff" d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
@@ -38,14 +75,7 @@ if (!isset($_SESSION['lg_in'])) {
         </button>
       </section>
 
-      <!-- frame movil -->
-      <section class="z-20 bg-grisva w-[80%] h-fit fixed top-[9%] left-[18%] rounded-xl p-6 items-center text-center hidden xl:hidden" id="items"> 
-        <a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[25px]">Acortar</button></a>
-        <a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[25px]">Mis acortadores</button></a>
-        <a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[25px]">Perfil</button></a>
-        <a href="#"><button class="rounded-full bg-prpl hover:bg-azulnav transtion duration-200 p-1 w-full mb-[5px]">Iniciar sesión</button></a>
 
-      </section>
       
     </nav>
     <div class="w-[100%] h-[91vh] bg-black pt-[2%]">
--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -6,6 +6,8 @@ tailwind.config = {
         rosanav: "#8341b8",
         prpl: "#3c32ff",
         grisva: "#141414",
+        malo: "#ad2424",
+        malodo: "#cf4747",
       },
       fontFamily: {
         sans: ["Poppins"],
